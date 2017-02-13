238c9a723f5a0eb983ad4ad3ba54461c1ef337f8
Zeldina Chalco <dazelcolab2016@gmail.com>
2017-02-13 13:23:09 -0500

    sing up

---
+++ js/json.js
@@ -0,0 +1,244 @@
+name, phone_code
+Afghanistan,93
+Albania,355
+Germany,49
+Algeria,213
+Andorra,376
+Angola,244
+Anguilla,1 264
+Antarctica,672
+Antigua and Barbuda,1 268
+Netherlands Antilles,599
+Saudi Arabia,966
+Argentina,54
+Armenia,374
+Aruba,297
+Australia,61
+Austria,43
+Azerbaijan,994
+Belgium,32
+Bahamas,1 242
+Bahrain,973
+Bangladesh,880
+Barbados,1 246
+Belize,501
+Benin,229
+Bhutan,975
+Belarus,375
+Myanmar,95
+Bolivia,591
+Bosnia and Herzegovina,387
+Botswana,267
+Brazil,55
+Brunei,673
+Bulgaria,359
+Burkina Faso,226
+Burundi,257
+Cape Verde,238
+Cambodia,855
+Cameroon,237
+Canada,1
+Chad,235
+Chile,56
+China,86
+Cyprus,357
+Vatican City State,39
+Colombia,57
+Comoros,269
+Congo,242
+Congo,243
+North Korea,850
+South Korea,82
+Ivory Coast,225
+Costa Rica,506
+Croatia,385
+Cuba,53
+Denmark,45
+Dominica,1 767
+Ecuador,593
+Egypt,20
+El Salvador,503
+United Arab Emirates,971
+Eritrea,291
+Slovakia,421
+Slovenia,386
+Spain,34
+United States of America,1
+Estonia,372
+Ethiopia,251
+Philippines,63
+Finland,358
+Fiji,679
+France,33
+Gabon,241
+Gambia,220
+Georgia,995
+Ghana,233
+Gibraltar,350
+Grenada,1 473
+Greece,30
+Greenland,299
+Guadeloupe,
+Guam,1 671
+Guatemala,502
+French Guiana,
+Guernsey,
+Guinea,224
+Equatorial Guinea,240
+Guinea-Bissau,245
+Guyana,592
+Haiti,509
+Honduras,504
+Hong Kong,852
+Hungary,36
+India,91
+Indonesia,62
+Iran,98
+Iraq,964
+Ireland,353
+Bouvet Island,
+Isle of Man,44
+Christmas Island,61
+Norfolk Island,
+Iceland,354
+Bermuda Islands,1 441
+Cayman Islands,1 345
+Cocos (Keeling) Islands,61
+Cook Islands,682
+Åland Islands,
+Faroe Islands,298
+South Georgia and the South Sandwich Islands,
+Heard Island and McDonald Islands,
+Maldives,960
+Falkland Islands (Malvinas),500
+Northern Mariana Islands,1 670
+Marshall Islands,692
+Pitcairn Islands,870
+Solomon Islands,677
+Turks and Caicos Islands,1 649
+United States Minor Outlying Islands,
+Virgin Islands,1 284
+United States Virgin Islands,1 340
+Israel,972
+Italy,39
+Jamaica,1 876
+Japan,81
+Jersey,
+Jordan,962
+Kazakhstan,7
+Kenya,254
+Kyrgyzstan,996
+Kiribati,686
+Kuwait,965
+Lebanon,961
+Laos,856
+Lesotho,266
+Latvia,371
+Liberia,231
+Libya,218
+Liechtenstein,423
+Lithuania,370
+Luxembourg,352
+Mexico,52
+Monaco,377
+Macao,853
+Macedonia,389
+Madagascar,261
+Malaysia,60
+Malawi,265
+Mali,223
+Malta,356
+Morocco,212
+Martinique,
+Mauritius,230
+Mauritania,222
+Mayotte,262
+Estados Federados de,691
+Moldova,373
+Mongolia,976
+Montenegro,382
+Montserrat,1 664
+Mozambique,258
+Namibia,264
+Nauru,674
+Nepal,977
+Nicaragua,505
+Niger,227
+Nigeria,234
+Niue,683
+Norway,47
+New Caledonia,687
+New Zealand,64
+Oman,968
+Netherlands,31
+Pakistan,92
+Palau,680
+Palestine,
+Panama,507
+Papua New Guinea,675
+Paraguay,595
+Peru,51
+French Polynesia,689
+Poland,48
+Portugal,351
+Puerto Rico,1
+Qatar,974
+United Kingdom,44
+Central African Republic,236
+Czech Republic,420
+Dominican Republic,1 809
+Rwanda,250
+Romania,40
+Russia,7
+Western Sahara,
+Samoa,685
+American Samoa,1 684
+Saint Barthélemy,590
+Saint Kitts and Nevis,1 869
+San Marino,378
+Saint Martin (French part),1 599
+Saint Pierre and Miquelon,508
+Saint Vincent and the Grenadines,1 784
+Ascensión y Tristán de Acuña,290
+Saint Lucia,1 758
+Sao Tome and Principe,239
+Senegal,221
+Serbia,381
+Seychelles,248
+Sierra Leone,232
+Singapore,65
+Syria,963
+Somalia,252
+Sri Lanka,94
+South Africa,27
+Sudan,249
+Sweden,46
+Switzerland,41
+Suriname,597
+Svalbard and Jan Mayen,
+Swaziland,268
+Tajikistan,992
+Thailand,66
+Taiwan,886
+Tanzania,255
+East Timor,670
+Togo,228
+Tokelau,690
+Tonga,676
+Trinidad and Tobago,1 868
+Tunisia,216
+Turkmenistan,993
+Turkey,90
+Tuvalu,688
+Ukraine,380
+Uganda,256
+Uruguay,598
+Uzbekistan,998
+Vanuatu,678
+Venezuela,58
+Vietnam,84
+Wallis and Futuna,681
+Yemen,967
+Djibouti,253
+Zambia,260
+Zimbabwe,263
